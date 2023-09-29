7563d2bdca5a5b09aa936a223111978031696531
Pierre Comte <p.s.comte@hotmail.fr>
2023-09-29 22:19:06 +0200

    ajout de dot !

--- assets/script.js
+++ assets/script.js
@@ -16,14 +16,27 @@ const slides = [
     image: "slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
 ];
 
 const bannerImg = document.querySelector(".banner-img");
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
-const dots = document.querySelectorAll(".dot");
-
+const dots = document.querySelector(".dots");
+// Initialisation de l'indice de la diapositive actuelle
 let currentIndex = 0;
+// Fonction pour mettre à jour le carrousel avec une la bonne img/text
 
 function updateCarousel(index) {
   if (index < 0) {
@@ -31,27 +44,49 @@ function updateCarousel(index) {
   } else if (index >= slides.length) {
     currentIndex = 0;
   }
-
+  // Récupère la diapositive actuelle en fonction de l'indice
   const slide = slides[currentIndex];
   bannerImg.src = `assets/images/slideshow/${slide.image}`;
   bannerImg.alt = `Slide ${currentIndex + 1}`;
   document.querySelector("p").innerHTML = slide.tagLine;
 
-  dots.forEach((dot, i) => {
-    dot.classList.toggle("dot_selected", i === currentIndex);
+  // Supprime la classe "dot_selected" de tous les points
+  const allDots = document.querySelectorAll(".dot");
+  allDots.forEach((dot) => {
+    dot.classList.remove("dot_selected");
   });
+
+  // Ajoute la classe "dot_selected" au point correspondant à la nouvelle diapositive
+  allDots[currentIndex].classList.add("dot_selected");
 }
 
+function generateDots() {
+  const dotsContainer = document.querySelector(".dots");
+  for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement("div");
+    dot.classList.add("dot");
+    // Ajoute la classe "dot_selected" au premier point
+    if (i === 0) {
+      dot.classList.add("dot_selected");
+    }
+    dotsContainer.appendChild(dot);
+  }
+}
+
+// Déclancheur gauche
 arrowLeft.addEventListener("click", () => {
   currentIndex--;
   updateCarousel(currentIndex);
+  console.log("updateGauche");
 });
-
+// Déclancheur droite
 arrowRight.addEventListener("click", () => {
   currentIndex++;
   updateCarousel(currentIndex);
 
   console.log("updateDroite");
 });
+
+generateDots();
 
 updateCarousel(currentIndex);
